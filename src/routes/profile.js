0d7341b225884166b9d4571ba98f3c205482e70f
pranaykargam <sunnykargam@gmail.com>
2025-07-26 19:10:17 +0530

    updated

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const profileRouter = express.Router();
 const {UserAuth} = require("../middlewares/auth");
 const {validateEditProfileData} = require("../utils/validation")
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
 
 profileRouter.get("/profile/view",UserAuth, async (req, res) => {
     try {
@@ -22,7 +24,9 @@ try{
 const loggedInUser = req.user;
 console.log(loggedInUser)
 
-Object.keys(req,body).forEach((key) => (loggedInUser[key] = req.body[key]))
+Object.keys(req.body).forEach((key) => {
+    loggedInUser[key] = req.body[key];
+  });
 console.log("LoggedInUser")
     await loggedInUser.save()
 
@@ -30,5 +34,41 @@ res.send(`${loggedInUser.firstName}, your profile updated successfully!!`)
   }catch(err){
     return res.status(400).send("ERROR")
 }})
+
+
+
+profileRouter.patch("/profile/password-forgot", async (req, res) => {
+  try {
+    const { emailId, newPassword } = req.body;
+
+    if (!emailId || !newPassword) {
+      return res.status(400).send("Email and new password are required!!!");
+    }
+
+    // Check if user exists
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    // Validate password strength (optional but recommended)
+    if (newPassword.length < 6) {
+      return res.status(400).send("Password must be at least 6 characters");
+    }
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update password
+    user.password = hashedPassword;
+    await user.save();
+
+    res.send("Password updated successfully!");
+  } catch (err) {
+    console.error("Forgot password error:", err.message);
+    res.status(500).send("Server error: " + err.message);
+  }
+});
+
 
 module.exports = profileRouter
