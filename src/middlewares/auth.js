b7313fef159e9506b96a2e5a4426f66294b896d9
pranaykargam <sunnykargam@gmail.com>
2025-08-08 03:31:02 +0530

    updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,25 +2,29 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 
-
-
-const UserAuth = (req, res, next) => {
-  const token = req.cookies.token; // ✅ read from cookies
-
-  if (!token) {
-    return res.status(401).send("Unauthorized: No token found in cookies");
-  }
-
+const UserAuth = async (req, res, next) => {
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // attach user info
+    const { token } = req.cookies;
+    if (!token) {
+     return res.status(401).send("please Login!!!");
+    }
+    console.log()
+
+    const decodedObj = jwt.verify(token, "DEV@tinder7680");
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id); // ✅ FIXED HERE
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user; // optional: attach user for downstream use
     next();
   } catch (err) {
-    return res.status(401).send("Unauthorized: Token is not valid!");
+    res.status(401).send("Unauthorized: " + err.message);
   }
 };
 
-module.exports = UserAuth;
 
 
 module.exports = {
