1b400cbf77a4dac01ca8443d23fe9d01deee129b
pranaykargam <sunnykargam@gmail.com>
2025-08-11 05:10:54 +0530

    updated

--- src/models/user.js
+++ src/models/user.js
@@ -119,3 +119,5 @@ userSchema.methods.validatePassword = async function (passwordInputByUser){
 
 module.exports = mongoose.model("User", userSchema);
 
+// module.exports = mongoose.model("User", userSchema);
+// module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);module.exports = mongoose.model("User", userSchema);
