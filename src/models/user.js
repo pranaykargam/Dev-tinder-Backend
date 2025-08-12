d53d8e2dab28e984df294e4c9d5b5dc6da5d11fd
pranaykargam <sunnykargam@gmail.com>
2025-08-12 08:24:44 +0530

    updated

--- src/models/user.js
+++ src/models/user.js
@@ -118,4 +118,33 @@ userSchema.methods.validatePassword = async function (passwordInputByUser){
 
 
 module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
+
 
