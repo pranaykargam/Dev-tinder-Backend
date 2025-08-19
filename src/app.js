3e618bae9a5730f94514a1cc44394e8c3b2c1100
pranaykargam <sunnykargam@gmail.com>
2025-08-19 21:44:04 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -196,18 +196,7 @@ app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+
 
 
 
